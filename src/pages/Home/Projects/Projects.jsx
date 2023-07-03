5d23415f79f88fc2aac8de0805c0414ff34bf134
Haa-meem <haameemalhamra@gmail.com>
2023-07-03 19:52:39 +0600

    updated details of My Projects

--- src/pages/Home/Projects/Projects.jsx
+++ src/pages/Home/Projects/Projects.jsx
@@ -42,28 +42,29 @@ const Projects = () => {
                             <button className="btn" onClick={() => openModal(project.id)}><FaInfo></FaInfo>Details</button>
                             <dialog id={project.id} className="modal modal-bottom sm:modal-middle">
                                 <form method="dialog" className="modal-box">
-                                    <h3 className="font-bold text-lg">Features
+                                    <h3 className="font-bold text-2xl">Features
                                     </h3>
-                                    <p className="py-4">Recipe database: The website contains a large collection of Chinese recipes from various chefs. Users can search for recipes by cuisine, ingredient, or chef.
-                                        Chef profiles: Each chef has a profile page that includes their bio, photo, and a list of their recipes. Users can follow their favorite chefs to stay updated on their latest creations.
-                                        Recipe ratings and reviews: Users can rate and review recipes to share their feedback with other users. This helps to promote the best recipes and encourages chefs to improve their cooking.
-                                        Recipe sharing: Chefs can easily upload their own recipes to the website, including photos and step-by-step instructions. This helps to grow the recipe database and allows chefs to showcase their skills.
-                                        Mobile-friendly: The website is optimized for mobile devices, so users can easily access it from their smartphones or tablets.
-                                        Technologies</p>
-                                    <h3 className="font-bold text-lg">
-                                        The website is built using the following technologies:
+                                    <ul className="list-disc ml-6 text-left">
+                                        {project.features.map((feature, index) => (
+                                            <li key={index}>{feature}</li>
+                                        ))}
+                                    </ul>
+                                    <h3 className="font-bold text-xl">
+                                        Technologies
                                     </h3>
-                                    <p className="py-4">
-                                        Front-end: React, Bootstrap
-                                        Back-end: Firebase</p>
+                                    <ul className="list-disc text-left ml-6">
+                                        {project.technologies.map((technology, index) => (
+                                            <li key={index}>{technology}</li>
+                                        ))}
+                                    </ul>
                                     <div className="modal-action">
                                         <button className="btn" onClick={() => closeModal(project.id)}>Close</button>
                                     </div>
                                 </form>
                             </dialog>
                             <div className="card-actions">
-                                <Link target="_blank" to={project.client}><button className="btn btn-outline btn-info btn-xs sm:btn-sm"><FaGithub></FaGithub>Server Side</button></Link>
-                                <Link target="_blank" to={project.server}><button className="btn btn-outline btn-success btn-xs sm:btn-sm"><FaGithub></FaGithub>Client Side</button></Link>
+                                <Link target="_blank" to={project.client}><button className="btn btn-outline btn-info btn-xs sm:btn-sm"><FaGithub></FaGithub>Client Side</button></Link>
+                                <Link target="_blank" to={project.server}><button className="btn btn-outline btn-success btn-xs sm:btn-sm"><FaGithub></FaGithub>Server Side</button></Link>
                                 <Link target="_blank" to={project.live}><button className="btn btn-outline btn-warning btn-xs sm:btn-sm"><FaExternalLinkAlt></FaExternalLinkAlt>Live Link</button></Link>
                             </div>
                         </div>
